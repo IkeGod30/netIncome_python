66097aab72bd586f64eedf7caf05707bdd132e32
unknown <anthoizu@yahoo.com>
2023-04-01 16:37:05 -0400

    first

---
+++ income.py
@@ -0,0 +1,13 @@
+def person():
+    first_name = "Charles"
+    last_name = "Jones"
+    position = "Plumber"
+    PTO = "On"
+    Remark = "PTO Status :"
+    Floor = "61"
+    Location = "Assigned Floor :"
+    print(first_name.upper(), last_name.upper())
+    print(Remark, PTO.upper())
+    print(Location, Floor)
+
+person()

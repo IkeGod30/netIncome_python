9873933ce6e0dccae303e04167af7a94fd69d226
unknown <anthoizu@yahoo.com>
2023-04-01 16:53:07 -0400

    Added f string literal to function

--- income.py
+++ income.py
@@ -1,12 +1,12 @@
 def person():
-    first_name = "Charles"
+    first_name = "Tony"
     last_name = "Jones"
     position = "Plumber"
     PTO = "On"
     Remark = "PTO Status :"
     Floor = "61"
     Location = "Assigned Floor :"
-    print(first_name.upper(), last_name.upper())
+    print(f"Welcome {first_name.upper()}, {last_name.upper()}")
     print(Remark, PTO.upper())
     print(Location, Floor)
 

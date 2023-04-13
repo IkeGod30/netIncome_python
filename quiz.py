09ef561b77e75273e86ecb58059f3252a127ecb2
unknown <anthoizu@yahoo.com>
2023-04-12 20:55:50 -0400

    Created a new program

---
+++ quiz.py
@@ -0,0 +1,16 @@
+iphone = "Things Fall Apart by Chinua Achebe"
+car = "Half of Yellow Sun Chimamanda Adichie"
+vacation = "Merchant of Venice by William Shakespear"
+israel = "Sanits In Hell by Tony Izuogu"
+
+if iphone:
+    print('quiz1')
+elif car:
+    print('quiz2')
+elif vacation:
+    print('quiz3')
+elif israel:
+    print('quiz4')
+else:
+    print('No quiz available')
+
